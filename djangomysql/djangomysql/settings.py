ca37270e1dd8c6df3575cc1192c16d8a73833315
kadd0ury <kaddoury2017@gmail.com>
2022-07-03 12:15:32 -0700

    change_setting.py

--- djangomysql/djangomysql/settings.py
+++ djangomysql/djangomysql/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+rudo)t63p8b_d0a1un$jcq(f6+vi40-deamj!c(lta5%&^sa9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.3.128', 'localhost', '127.0.0.1']
 
 
 # Application definition
